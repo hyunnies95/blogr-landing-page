a3c840ec0863d6d01a20c19d88a9455301a6e02d
hyunnies95 <hyunnies95@gmail.com>
2021-06-29 14:31:46 +0500

    mobile menu animation

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const hamburger = document.querySelector('.header__hamburger'),
       menu = document.querySelector('.header__links')
 
 hamburger.addEventListener('click', function() {
-  menu.classList.toggle('open')
+  if (menu.classList.contains('slideout')) {
+    menu.classList.remove('slideout')
+    menu.classList.add('slidein')
+  } else if (menu.classList.contains('slidein')) {
+    menu.classList.remove('slidein')
+    menu.classList.add('slideout')
+  } else {
+    menu.classList.add('slidein')
+  }
+
   hamburger.classList.toggle('open')
 })
